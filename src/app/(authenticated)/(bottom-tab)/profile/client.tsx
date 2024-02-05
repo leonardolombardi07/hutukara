cfe7ee2668bfaecce5cf6335c2651de945f92805
Leonardo Lombardi <leonardolombardi@poli.ufrj.br>
2024-02-05 13:43:33 +0000

    add metadata to profile page

---
+++ src/app/(authenticated)/(bottom-tab)/profile/client.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import Box from "@mui/material/Box";
+import Header, { HeaderSkeleton } from "./_page/Header";
+import Container from "@mui/material/Container";
+import PersonalPreferences from "./_page/PersonalPreferences";
+import { useUser } from "@/app/_layout/UserProvider";
+import { useFirestoreUser } from "@/modules/api/client";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import useDelay from "@/modules/hooks/useDelay";
+
+export default function ClientPage() {
+  const { user: authUser } = useUser();
+  const [firestoreUser, _, error] = useFirestoreUser();
+
+  const delayedIsLoading = useDelay(!Boolean(firestoreUser));
+
+  return (
+    <Container
+      component={"main"}
+      sx={{
+        py: 4,
+      }}
+    >
+      {error && (
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          {error?.message}
+        </Alert>
+      )}
+
+      {delayedIsLoading ? (
+        <HeaderSkeleton />
+      ) : (
+        <Header
+          displayName={firestoreUser?.name}
+          email={authUser.email}
+          photoURL={firestoreUser?.photoURL}
+        />
+      )}
+
+      <Box sx={{ my: 3 }} />
+      <PersonalPreferences />
+    </Container>
+  );
+}
