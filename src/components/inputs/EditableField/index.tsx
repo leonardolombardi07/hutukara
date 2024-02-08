1e7ee778b89b3d33b16ea49ce788438a852a59d1
Leonardo Lombardi <leonardolombardi@poli.ufrj.br>
2024-02-08 09:28:11 +0000

    default value on editable field

--- src/components/inputs/EditableField/index.tsx
+++ src/components/inputs/EditableField/index.tsx
@@ -38,6 +38,7 @@ export default function EditableField({
         </Typography>
       ) : (
         <TextField
+          defaultValue={value}
           variant={textFieldProps?.variant || "outlined"}
           autoFocus
           onBlur={(event) => {
