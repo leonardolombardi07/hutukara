d386bd7beb97b9a1e7bf66c1acfdd553158b3d06
Leonardo Lombardi <leonardolombardi@poli.ufrj.br>
2024-02-15 16:25:34 +0000

    fix no needed score on input content

--- src/modules/api/types.ts
+++ src/modules/api/types.ts
@@ -49,7 +49,7 @@ export namespace GroupsCol {
       export interface Doc {
         group: GroupsCol.Doc;
         allMembers: Omit<WithId<UsersCol.Doc>, "groupIds">[];
-        content: (WithId<ContentCol.Doc> & { score: number })[];
+        content: WithId<ContentCol.Doc>[];
         ratings: UsersCol.RatingsSubCol.Doc[];
       }
     }
